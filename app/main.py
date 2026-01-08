a6d7bd30acd2174fa1e408f5f33f0a4829076ddf
Wagner dos Santos Oliveira <wagner.oliveira@fvoalimentos.com.br>
2026-01-07 21:58:58 -0300

    update nas rotas e add chamada bot whatsapp v3

--- app/main.py
+++ app/main.py
@@ -3,15 +3,13 @@ from fastapi.staticfiles import StaticFiles
 from app.database import Base, engine
 from app.routes.checklist import router as checklist_router
 
-app.include_router(checklist_router)
-
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title="Checklist Veicular MVP",
     version="1.0.0"
 )
 
-#app.include_router(router)
+Base.metadata.create_all(bind=engine)
+
+app.include_router(checklist_router)
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
--- app/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from app.routes.checklist import router as checklist_router
-
-router = APIRouter()
-router.include_router(checklist_router)
